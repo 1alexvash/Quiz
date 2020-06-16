8b86d5cbdf8c49746caa2d7cbe38efc370beeb59
Alexander <1alexvash@gmail.com>
2020-06-16 11:29:49 +0300

    Updates

---
+++ precache-manifest.7d56ca2e4b9a2fed385823291f2eaa8a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1019c61056fad0dbb2accb4061432985",
+    "url": "/Quiz/index.html"
+  },
+  {
+    "revision": "04124122d1cde581a50d",
+    "url": "/Quiz/static/css/main.1f0c7086.chunk.css"
+  },
+  {
+    "revision": "9f4372f3d0d6f3cdef5d",
+    "url": "/Quiz/static/js/2.9d268d3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Quiz/static/js/2.9d268d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04124122d1cde581a50d",
+    "url": "/Quiz/static/js/main.c9d17a2d.chunk.js"
+  },
+  {
+    "revision": "d97f62eb5c1ec7a787a6",
+    "url": "/Quiz/static/js/runtime-main.621570c9.js"
+  },
+  {
+    "revision": "f1e343cfe7e5e7926dab3f6da21f9377",
+    "url": "/Quiz/static/media/checkmark.f1e343cf.png"
+  },
+  {
+    "revision": "335ef8acf76accf9a4aabbcfc113818a",
+    "url": "/Quiz/static/media/rectangle.335ef8ac.png"
+  },
+  {
+    "revision": "7768c1d08a2ccb9be8d865eb81b94993",
+    "url": "/Quiz/static/media/sound_failure.7768c1d0.mp3"
+  },
+  {
+    "revision": "dcbb5b439d236a873f337fc600cf260c",
+    "url": "/Quiz/static/media/sound_success.dcbb5b43.mp3"
+  },
+  {
+    "revision": "3d34c88004c1f6546b960f9c0352759e",
+    "url": "/Quiz/static/media/timer.3d34c880.png"
+  }
+]);
